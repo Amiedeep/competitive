74a32c32351acc3d590a995cc59f04aa7304f9fa
Amandeep Singh <amandees@thoughtworks.com>
2018-11-04 23:49:28 +0530

    CHFTIRED problem november challenge 18

---
+++ src/November18/CHFTIRED.java
@@ -0,0 +1,18 @@
+package November18;
+
+import java.util.Scanner;
+
+public class CHFTIRED {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOftestCases = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < numberOftestCases; i++) {
+            scanner.nextLine();
+        }
+
+        for (int i = 0; i < numberOftestCases; i++) {
+            System.out.println("YES");
+        }
+    }
+}
